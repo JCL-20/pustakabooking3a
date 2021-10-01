24df83514b9ee6cad9e0fe586704bb74b74c0529
JCL-20 <12200528@bsi.ac.id>
2021-10-01 12:23:22 +0700

    Project Matakuliah

---
+++ application/controllers/Latihan1.php
@@ -0,0 +1,13 @@
+<?php
+class Latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Selamat Datang di WP2";
+
+    }
+    public function halo()
+    {
+        echo "halo";
+    }
+}
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,14 +1,21 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller 
+{
 
 	public function index()
 	{
-		echo"<h1>Perkenalan <h1>";
-		echo"Nama              :Uzi Jonathan Tuhuleruw <br>
-		     Tanggal Lahir     :Bekasi 21 Desember 2002 <br>
-			 Tempat Tinggal    :Perumahan Kompas Indah, Jln. Meranti Blok c24 no.11, Kota Bekasi, Kabupaten Tambun Selatan, Provinsi Jawa Barat, Kode Pos 17510 <br>
-             Motivation Kuliah :Ingin menempuh Pendidikan yang lebih Tinggi";
-     }
+		$this->load->view('view-latihan');
+
+	}
+	public function penjumlahan($n1, $n2)
+	{
+		$this->load->model('Model_latihan1');
+		$hasil = $this->Model_latihan1->jumlah ($n1, $n2);
+		echo"Hasil Penjumlahan dari " . $n1 . "+" . $n2 . "=" . $hasil;
+
+		$this->load->view('view-latihan', $hasil);
+	}
+   
 }
