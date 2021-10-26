3a28c1ab9b3e04a62a359a7cdd1545cd5028cc8e
JCL-20 <12200528@bsi.ac.id>
2021-10-26 15:20:51 +0700

    crud

--- application/views/v_form.php
+++ application/views/v_form.php
@@ -1,26 +1,27 @@
-<?php
-class Form extends CI_Controller
-{
-     function __construct()
-     {
-           parent::__construct();
-           $this->load->library('form_validation');
-     }
-     function index()
-     {
-          $this->load->view('v_form');
-     }
-     function aksi()
-     {
+<!DOCTYPE html>
+<html>
 
-          $this->form_validation->set_rules('nama','Nama','required');
-          $this->form_validation->set_rules('email','Email','required');
-          $this->form_validation->set_rules('konfir_email','Konfirmasi email','required');
+<head>
+    <title>Membuat Form Validation dengan Codeigniter</title>
+</head>
 
-          if ($this->form_validation->run() !=false) {
-               echo "Form Validation oke";
-          } else {
-               $this->load->view('v_form');
-          }
-     }
-}
+<body>
+    <form action="<?= base_url('form/aksi'); ?>" method="post">
+        <h1>Membuat Form Validation dengan Codeigniter</h1>
+        <?php echo validation_errors(); ?>
+        <?php echo form_open('form/aksi'); ?>
+        <label>Nama</label><br />
+        <input type="text" name="nama"><br />
+        <?php echo form_error('nama'); ?>
+        <label>Email</label><br />
+        <input type="text" name="email"><br />
+        <?php echo form_error('email'); ?>
+        <label>Konfirmasi Email</label><br />
+        <input type="text" name="konfir_email"><br />
+        <?php echo form_error('konfir_email'); ?>
+        <input type="Submit" value="Simpan">
+
+    </form>
+</body>
+
+</html>
--- application/views/v_tampil.php
+++ application/views/v_tampil.php
@@ -1,37 +1,39 @@
 <html>
 
 <head>
-        <title>Crud dengan CodeIgniter</title>
-<head>
+    <title>Crud dengan CodeIgniter</title>
+</head>
+
 <body>
     <center>
-        <h1>Membuat Sistem CRUD dengan CodeIgniter </h1>
+        <h1>Membuat Sistem CRUD dengan CodeIgniter</h1>
     </center>
     <center><?php echo anchor('crud/tambah', 'Tambah Data'); ?></center>
-    <tabele style="Margin: 20px auto;" border="1">
-          <tr>
-              <th>No</th>
-              <th>Nama</th>
-              <th>Alamat</th>
-              <th>Pekerjaan</th>
-              <th>Action</th>
-          </tr>
-          <?php
-          $no = 1;
-          foreach ($user as $u)
-          ?>
-          <tr>
-              <td><?php echo $no++ ?></td>
-              <td><?php echo $u->nama ?></td>
-              <td><?php echo $->alamat ?></td>
-              <td><?php echo $->pekerjaan ?></td>
-              <td>
-                  <?php echo acnhor('crud/edit/' .$u->id, 'Edit'); ?>
-                  <?php echo acnhor('crud/Hapus/' $u->id, 'Hapus'); ?>
-              </td>
-          </tr>
+    <table style="margin: 20px auto;" border="1">
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Alamat</th>
+            <th>Pekerjaan</th>
+            <th>Action</th>
+        </tr>
+        <?php
+        $no = 1;
+        foreach ($user as $u) {
+        ?>
+            <tr>
+                <td><?php echo $no++ ?></td>
+                <td><?php echo $u->nama ?></td>
+                <td><?php echo $u->alamat ?></td>
+                <td><?php echo $u->pekerjaan ?></td>
+                <td>
+                    <?php echo anchor('crud/edit/' . $u->id, 'Edit'); ?>
+                    <?php echo anchor('crud/hapus/' . $u->id, 'Hapus'); ?>
+
+                </td>
+            </tr>
         <?php } ?>
-    </tabel>
-</body>
+    </table>
+</body> 
 
 </html>
